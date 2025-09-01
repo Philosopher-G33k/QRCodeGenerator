ccc33c8b24803947b6fafb6cc37a58c09569083d
ishan malviya <ishan.malviya@gmail.com>
2025-09-01 10:25:05 +0530

    Initial Commit

---
+++ QRCodeGenerator/QRCodeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeGeneratorApp.swift
+//  QRCodeGenerator
+//
+//  Created by Ishan Malviya on 01/09/25.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
